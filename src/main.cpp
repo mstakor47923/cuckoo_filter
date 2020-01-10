2e9a3fbf309e418ada6e7589a366c120434e79bb
Dominik Kotarski <dojitza@gmail.com>
2020-01-10 14:46:48 +0100

    Data output WIP

--- src/main.cpp
+++ src/main.cpp
@@ -16,29 +16,32 @@ int main(int argc, char *argv[]) {
 		return 1;
 	};
 
-	// ------ for generating test files ------
-	std::vector<std::pair<uint32_t, uint32_t>> fileGenInfo = {
-	std::make_pair(100, 10000), 
-	std::make_pair(50, 10000)
-	};
-	//generateTestFiles(argv[1], fileGenInfo);
+	if (argc > 2){
+		// ------ for generating test files ------
+		std::vector<std::pair<uint32_t, uint32_t>> fileGenInfo = {
+		std::make_pair(100, 1000), 
+		std::make_pair(50, 1000)
+		};
+		generateTestFiles(argv[1], fileGenInfo);
+		return 0;
+	}
 	
 
 	uint32_t bucketSize = 4;
-	uint32_t bucketNumber = 25000;
+	uint32_t bucketNumber = 2500;
 	uint32_t fingerprintSize = 11;
 	uint32_t maxNumberOfKicks = 5;
+	uint32_t numberOfTestKmers = 100000;
+
 
 	cuckoo::CuckooHashing* hashingAlg = new cuckoo::CuckooHashing(bucketNumber);
 	cuckoo::Filter* fltr = new cuckoo::DynamicCuckooFilter(bucketSize, bucketNumber, fingerprintSize, maxNumberOfKicks, hashingAlg);
 
 	uint16_t kSize = 100;
 	uint32_t kCount = 10000;
-	auto kmers = cuckoo::DataLoader::loadFromFile(argv[1], kSize, kCount);
-	auto mutatedKmers = generateNeKmers(1000, kmers);
-	for (auto it = mutatedKmers->begin(); it != mutatedKmers->end(); ++it){
-		std::cout << (*it) << std::endl;
-	}
+	auto kmers = cuckoo::DataLoader::loadKmersFromFile(argv[1]);
+	auto mutatedKmers = generateNeKmers(numberOfTestKmers, kmers);
+
 
 	auto start = std::chrono::system_clock::now();
 	for (auto it = kmers->begin(); it != kmers->end(); it++) {
@@ -48,6 +51,16 @@ int main(int argc, char *argv[]) {
 	auto end = std::chrono::system_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 	std::cout << "Insert duration: " << elapsed.count() << "ms" << std::endl;
+	int fp(0);
+	for (auto it = mutatedKmers->begin(); it != mutatedKmers->end(); ++it){
+		if (fltr->lookup(*it)){
+			//std::cout << "False positive: " << *it  << std::endl;
+			++fp;
+		}
+		
+	}
+	std::cout << "Fp rate: " << (float)fp/numberOfTestKmers << std::endl;
+	std::cout << "Memory taken " << bucketNumber*(sizeof(uint32_t)+bucketSize*sizeof(uint32_t)) << std::endl;
 
 	delete fltr;
 	delete hashingAlg;
