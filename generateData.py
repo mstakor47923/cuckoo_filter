de9556d194ff554cc0e9ae9882faa764eaf105f3
Dominik Kotarski <dojitza@gmail.com>
2020-01-15 15:02:44 +0100

    Data generation script updated

--- generateData.py
+++ generateData.py
@@ -5,13 +5,14 @@ kmernums = [1000, 10000, 100000, 1000000]
 
 
 l= []
-for i in kmersizes:
-    for j in kmernums:
-        l.append((i,j))
+for i in kmernums:
+    for j in kmersizes:
+        l.append((j,i))
 
 for kmersize, kmernum in l:
     filename = 'kmers/{}mer-{}.txt'.format(str(kmersize), str(kmernum))
     print('Now working on' + filename)
     args = ('./cuckoo_filter', filename, str(kmersize), str(kmernum), str(kmernum/4))
     popen = subprocess.Popen(args)
+    popen.wait()
 print('ScriptDone')
